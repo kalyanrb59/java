8ab9b5e296fbc133bacfee3dd3e8608c5d3281a5
kalyanrb59 <kalylanrb59@gmail.com>
2026-03-15 19:38:28 +0530

    if-else statement

---
+++ javaStmts/ifelse2.java
@@ -0,0 +1,9 @@
+void main(){
+    if(10<20){
+        System.out.println("Hello Kalyan");
+        System.out.println("Hello Edupoly");
+    }
+}
+// //because we created block, all the statements within the block 
+// comes under one statement. thes kind of statement is called 
+// compound statement
---
+++ javaStmts/ifelseScanner.java
@@ -0,0 +1,13 @@
+void main(){
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+    if(x>y){
+        System.out.println("Hello Edupoly");
+        System.out.println("Hello Kalyan Chakravarthi");
+    }
+}
+
+// if the condition's following statement is compound statement(with in a block)
+// if the condition is true it will execute the compound statement(all statements in the block)
+// if the condition is false it will not execute the compound statement
