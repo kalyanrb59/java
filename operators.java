1f8bc9b70df87a782861187738eebd7c251ee62b
kalyanrb59 <kalyanrb59@gmail.com>
2026-03-15 12:00:34 +0530

    modified

--- operators.java
+++ operators.java
@@ -1,5 +1,5 @@
 void main(){
-    int a = 10;
+    int a = 10 ;
     int b = 20;
     // int c = a++;
     // int d = ++a;
---
+++ relationalOperators.java
@@ -0,0 +1,10 @@
+void main(){
+    int a = 14;
+    int b = 20;
+    double d = 20.0;
+    boolean c = a<b;
+    String e = "14";
+    // System.out.println(a==e);  //incompatable operand types int and string
+    System.out.println(c);
+    System.out.println(b==d);
+}
