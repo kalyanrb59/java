f7838d5ca6157b2abc2c25feb04a4f2ee5732b42
kalyanrb59 <kalyanrb59@gmail.com>
2026-03-15 09:15:57 +0530

    modified

--- operators.java
+++ operators.java
@@ -1,9 +1,17 @@
 void main(){
     int a = 13;
     int b = 20;
+    // int c = a++;
+    // int d = ++a;
+    int e = a++ + a++;
+    int f = a++ + ++a;
     System.out.println(a/b); //Res: 0 
     System.out.println((double) a/b); // res 0.5
     System.out.println((double) (a/b)); // res 0.0
     System.out.println((double) a/(double) b); // res 0.5
     System.out.println(a%b); //res 13
+    // System.out.println(c);
+    // System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
 }
