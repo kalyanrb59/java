a1901b9a831139643e0bdb0c11b5cfbe3ed8f5e5
kalyanrb59 <kalylanrb59@gmail.com>
2026-03-15 19:21:31 +0530

    if-else

---
+++ javaStmts/ifelse.java
@@ -0,0 +1,10 @@
+void main(){
+    if(10<20)
+    System.out.println("hello edupoly"); // if controls only the following statement
+    System.out.println("Hello Kalayan");
+
+    // if controls only the following statement. if the condition is 
+    // true, the following statement will be executed 
+    // if the condition is false it will block the following statement only.
+    // the rest of the statements will be executed.
+}
