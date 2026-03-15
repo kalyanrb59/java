40e3e62b832dfc8cf566d2fd5c45695daa075a71
kalyanrb59 <kalyanrb59@gmail.com>
2026-03-15 08:20:02 +0530

    my first commit

---
+++ Second.java
@@ -0,0 +1,10 @@
+class Abc{
+    public static void main(String[] args){
+        System.out.println("Hello Abc");
+    }
+}
+public class Second{
+    public static void main(String[] args){
+        System.out.println("Hello Java World");
+    }
+}
---
+++ declaration_datatypes.java
@@ -0,0 +1,7 @@
+void main(){
+  
+    int a = 20;
+    int b = 30;
+    System.out.println("The addition of " + a +" and " +b +" is " + (a+b) );
+    //System.out.println(STR."the addition of /{a} and /{b} is /{a+b}")
+}
