d237ed6aaa58fbc240be127017e7235dc84d781f
kalyanrb59 <kalyanrb59@gmail.com>
2026-03-17 20:47:32 +0530

    array values

---
+++ functions/array.java
@@ -0,0 +1,13 @@
+void main(){
+    int a = 99;
+    int[] n = {4,6,8,12,17,22};
+    // System.out.println(n[0]);
+    // System.out.println(a);
+    // System.out.println(n.length);
+
+    //print all the elements of the array.
+
+    for(int i=0;i<n.length-1; i++){
+        System.out.println(n[i]);
+    }
+}
